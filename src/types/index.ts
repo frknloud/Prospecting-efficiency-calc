a8d7ae8744ec1c0552e602b4ffeaa8043cffb821
frknloud <jorge.d.flores92@gmail.com>
2026-05-08 10:28:55 -0400

    Add shared stat and build type definitions

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,49 @@
-export {};
+export interface Stats {
+  luck: number;
+  capacity: number;
+  shakeStrength: number;
+  shakeSpeed: number;
+  digStrength: number;
+  digSpeed: number;
+}
+
+export interface EquipmentItem {
+  id: string;
+  name: string;
+  slot: string;
+  stats: Partial<Stats>;
+}
+
+export interface Mutation {
+  id: string;
+  name: string;
+  multipliers?: Partial<Stats>;
+  flat?: Partial<Stats>;
+}
+
+export interface Enchant {
+  id: string;
+  name: string;
+  stats?: Partial<Stats>;
+}
+
+export interface MuseumMineral {
+  id: string;
+  name: string;
+  multipliers?: Partial<Stats>;
+  flat?: Partial<Stats>;
+}
+
+export interface MuseumModifier {
+  id: string;
+  name: string;
+  multipliers?: Partial<Stats>;
+  flat?: Partial<Stats>;
+}
+
+export interface BuildResult {
+  finalStats: Stats;
+  efficiency: number;
+  cycleTime: number;
+  digsRequired: number;
+}
